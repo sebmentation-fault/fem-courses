13019b23e79da106dfb831c7ae785c171e29880f
Sebastian Kjällgren <sebastian@kjallgren.com>
2024-05-04 11:08:32 +0100

    feat: enums pt.2

--- polyglot-programming/src/main.rs
+++ polyglot-programming/src/main.rs
@@ -1,9 +1,21 @@
 enum RSEnum {
-    Foo,
-    Bar,
-    Baz,
+    Foo(fn() -> i32),
+    Foo2(Option<i32>),
+    Bar(String),
+    Baz(Vec<String>),
+}
+
+fn bar -> i32 {
+    return 5;
 }
 
 fn main() {
+    let foo = RSEnum::Foo(bar);
 
+    match foo {
+        RSEnum::Foo(fn_value) => {},
+        RSEnum::Foo2(Some(value)) => {},
+        RSEnum::Foo2(None) => {},
+        _ => {},
+    }
 }
