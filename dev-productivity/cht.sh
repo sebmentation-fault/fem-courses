0e39fcad65f069b6006e71cc56bf57f8dd76dfcb
Sebastian Kjällgren <sebastian@kjallgren.com>
2024-06-07 14:35:45 +0100

    feat: some more...

--- dev-productivity/cht.sh
+++ dev-productivity/cht.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 languages=$(echo "golang c cpp typescript java rust zig haskell python" | tr " " "\n")
-core_utils=$(echo "git tr xargs sort cat uniq curl find xargs sed awk pacman paru" | tr " " "\n")
+core_utils=$(echo "echo grep read head tail git tr xargs sort cat uniq curl find xargs sed awk pacman paru" | tr " " "\n")
 selected=$(echo -e "$languages\n$core_utils" | fzf)
 
 read -p "Query: " query
